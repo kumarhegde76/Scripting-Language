37f39036df3dcc2be6fd3b2ce39c177aef1c829f
Kumar Hegde <kumarhegde81@gmail.com>
2019-09-05 22:19:05 +0530

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,14 @@
+
+a ={}
+while(True):
+	print("Enter reg no,-1 to exit")
+	m = int(input())
+	if m==-1:
+		break
+	else:
+	     print("Enter name")
+	     n = input()
+	     a[m] = n
+for k,v in a.items():
+	if k%2==0:
+		print(v)	  
