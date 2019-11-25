e152d9f3318c966a73abae9408cf96d883594cc9
Kumar Hegde <kumarhegde81@gmail.com>
2019-11-25 21:53:41 +0530

    Delete first.py

--- first.py
+++
@@ -1,6 +0,0 @@
-mydictionary ={"name":"Archie", "identity":"Student","age":17}
-print(mydictionary)
-key=mydictionary["name"]
-value=mydictionary.get("name")
-print ("key is ",key)
-print("Value is",value)
